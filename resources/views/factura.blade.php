3b230ef9eb8ddede492418dee9249c895ef3024a
pabloleon1 <pablodefaz@gmail.com>
2025-08-04 23:38:43 -0500

    Update factura.blade.php

--- resources/views/factura.blade.php
+++ resources/views/factura.blade.php
@@ -138,7 +138,7 @@
                                min="0" 
                                max="100" 
                                step="0.01"
-                               value="{{ old('porcentaje_iva', isset($datos) ? $datos['porcentaje_iva'] : '12') }}"
+                               value="{{ old('porcentaje_iva', isset($datos) ? $datos['porcentaje_iva'] : '15') }}"
                                class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent">
                     </div>
                     
